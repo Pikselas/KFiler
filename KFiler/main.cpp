1e7491d657fcea14366d107913902609bdbf761f
kashikichi <60090083+Pikselas@users.noreply.github.com>
2021-12-01 20:34:17 +0530

    Explained

--- KFiler/main.cpp
+++ KFiler/main.cpp
@@ -6,19 +6,30 @@ int main()
 {
 	try
 	{
+		//creating a sender 
 		FileSender fs("1234");
+		
+		//creating a receiver 
 		FileReceiver fr;
 		
-		fs.IncreaseThread("1235");
-		fs.IncreaseThread("1236");
+		//adding file for sending
+		fs.AddFile("C:/Users/Aritra Maji/Downloads/Umiko_Ahagon_X_Sakura_Nene.png");
 		
+		//increasing channel for sender with a port ->defaults to 0
+		fs.IncreaseThread("1235");
+		
+		//increasing channel for receiver (no port need to be added for receiver)->dfaults to 0
 		fr.IncreaseThread();
-		fr.IncreaseThread();
+
+		//setting sender's address and port
 		fr.SetSender(NetworkBuilder::GetDeviceIPs()[0], "1234");
 		
+		//sending file via child thread
 		std::thread(&FileSender::StartTransfer,&fs).detach();
-
+		
+		//receiving file via main thread
 		fr.StartTransfer();
+		
 		std::cin.get();
 		
 	}
